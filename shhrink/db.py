0b2f60a0e226efb723a6245cc394c66451a76b7f
tfaughnan <tfaughnan13@gmail.com>
2021-02-12 18:31:16 -0500

    Handle form POST requests

---
+++ shhrink/db.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+import click
+
+def get_db():
+    pass
+
+
---
+++ shhrink/shhrink.py
@@ -0,0 +1,44 @@
+import functools
+from random import randrange
+
+# TODO: reevaluate which imports are actually needed
+from flask import Blueprint, flash, redirect, render_template, request, session, url_for
+
+from shhrink.db import get_db
+
+bp = Blueprint('shhrink', __name__)
+
+# TODO: is GET really necessary?
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+    urlout=''
+    if request.method == 'POST':
+        urlin = request.form['urlin']
+        db = get_db()
+        error = None
+        
+        if not urlin:
+            error = 'enter a url'
+        # TODO: also check if url is *valid*, e.g. by pinging it or something idk
+        elif False:
+            error = 'invalid url'
+
+        if error is None:
+            # TODO: do the shortening and add to database
+            print('we good homie')
+            print(f'INPUT: {urlin}')
+            urlout = shorten_url(urlin)
+            print(f'OUTPUT: {urlout}')
+        else:
+            print('nah fam that aint it')
+            flash(error)
+
+    return render_template('index.html', urlout=urlout)
+
+def shorten_url(url):
+    BASE = 'https://shhr.ink/'
+    key = str(randrange(0, 1000)).zfill(3)
+    return BASE + key
+    
+
+
