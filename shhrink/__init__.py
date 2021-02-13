63066e7c7fa13006326192f4afe08eecd8808ec5
tfaughnan <tfaughnan13@gmail.com>
2021-02-12 19:17:17 -0500

    Display error flashes

--- shhrink/__init__.py
+++ shhrink/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-            SECRET_KEY = None,
+            SECRET_KEY = 'dev',
             DATABASE = os.path.join(app.instance_path, 'shhrink.sqlite'),
             )
 
