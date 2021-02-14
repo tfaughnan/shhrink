631b41bcafc05ab9dff5f52e303b1aebb6f9e885
tfaughnan <tfaughnan13@gmail.com>
2021-02-13 19:55:19 -0500

    Outline db utils

--- shhrink/db.py
+++ shhrink/db.py
@@ -5,4 +5,14 @@ import click
 def get_db():
     pass
 
+def add_entry(urlin, urlout, timestamp):
+    pass
+
+def lookup_entry(urlin):
+    if urlin in db:
+        return urlout
+    else:
+        return None
+
+
 
