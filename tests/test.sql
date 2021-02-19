3d560672f46ae07adb002d8048534b59f846a2bf
tfaughnan <tfaughnan13@gmail.com>
2021-02-19 18:32:51 -0500

    Create tests directory

---
+++ tests/test.sql
@@ -0,0 +1,5 @@
+INSERT INTO shhrink (id, time, urlin, urlout, clicks)
+VALUES
+    (1, 0, 'https://flask.palletsprojects.com/en/1.1.x/testing/', 'https://shhr.ink/tH0', 0),
+    (2, 0, 'https://docs.pytest.org/en/stable/contents.html', 'https://shhr.ink/F4u, 1);
+
