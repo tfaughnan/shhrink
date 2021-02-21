5c5421330c5963e5e89153cd98f58b3742c47c5f
tfaughnan <tfaughnan13@gmail.com>
2021-02-20 22:37:00 -0500

    Clean up copyUrlout

--- shhrink/static/index.js
+++ shhrink/static/index.js
@@ -1,8 +1,6 @@
 function copyUrlout() {
-    console.log("function worked!");
     var copyText = document.getElementById("urlout");
     copyText.select();
     document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
 }
 
